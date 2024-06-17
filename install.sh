aadfd6622be6e0eaa9d636ebd7ac254a2c5c08db
meera malhotra <memalhot@redhat.com>
2024-06-17 18:32:06 -0400

    automating path installation, if path was not already added

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 current_dr=$(pwd)/bin
-echo "run the following command to add the ope to your path"
-echo "export PATH=$current_dr:\$PATH"
 
+#if path was not already added, add path
+if ! grep -q -s "export PATH=$current_dr:\$PATH" ~/.bashrc; then
+  echo "export PATH=$current_dr:\$PATH" >> ~/.bashrc #add it to path
+  source ~/.bashrc #rerun bashrc to reset shell
+fi
+
+#tell user ope tool has been installed
+echo "ope has been added to the path successfully"
